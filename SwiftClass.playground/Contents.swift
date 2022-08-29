80f1113273e5b7797bfa97cd121d3ad5385fbe71
Franklin Aurelio <franklin.carvalho@unifesp.br>
2022-08-29 19:07:07 -0300

    feat: swift class

---
+++ SwiftClass.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var name:String = ""
+var happiness:String = ""
+
+class Snack{
+    var name:String?
+    var happiness:String?
+}
+
+let snack:Snack = Snack()
+snack.name = "Rice"
+print(snack.name!)
