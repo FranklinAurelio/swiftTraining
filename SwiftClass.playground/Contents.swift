93741fd23dce74da2c04508b87dc9ce4d0e5c41e
Franklin Aurelio <franklin.carvalho@unifesp.br>
2022-08-30 17:08:21 -0300

    feat: swift Class II

--- SwiftClass.playground/Contents.swift
+++ SwiftClass.playground/Contents.swift
@@ -11,3 +11,32 @@ class Snack{
 let snack:Snack = Snack()
 snack.name = "Rice"
 print(snack.name!)
+
+//other ways to extract value from an optional variable
+if snack.name != nil{
+    print(snack.name!)
+}
+
+if let snackValue = snack.name{
+    print(snackValue)
+}
+
+func seeSnackName(){
+    if let name = snack.name{
+        print(name)
+    }
+    guard let name = snack.name else{
+        return
+    }
+    print(name)
+}
+
+seeSnackName()
+
+let number:Int? = Int("5")
+print(number)
+
+if let numberValue = number {
+    print(numberValue)
+}
+
